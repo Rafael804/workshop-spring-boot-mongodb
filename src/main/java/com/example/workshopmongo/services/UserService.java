377c0e2c2922d76ac89d6cd7b160b7cd6ecb8efe
Rafael Imamura Bortoni <rafaelimamura804@gmail.com>
2023-06-10 13:44:47 -0300

    Obtendo um usuario por id

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -7,13 +7,23 @@ import org.springframework.stereotype.Service;
 
 import com.aula.course.entities.User;
 import com.example.workshopmongo.repository.UserRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;	
+	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if 	(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
