6143b6fe29f4bd809f561f7a6afc5f3065d7f430
Rafael Imamura Bortoni <rafaelimamura804@gmail.com>
2023-06-11 14:13:04 -0300

    Obtendo post por id

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;	
+	
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if 	(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
